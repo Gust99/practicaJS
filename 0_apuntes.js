4dc96707986bb291cff6afd6994963ad52340eb9
unknown <gustavogud77@gmail.com>
2022-02-10 17:59:27 -0400

    practica 1 1

--- 0_apuntes.js
+++ 0_apuntes.js
@@ -1,5 +1,8 @@
 /*
 	MIT LICENSE es la licencia mas basica
+
 	NODE es un template para el .gitignore (.nombre para carpetas, nombre para archivos)
+	
 	README.md se utiliza para brindar una descripcion detallada del repositorio
+	Utiliza un lenguaje llamado markdown
 */
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # practicaJS
+
+## Taller 1: figuras geometricas
+-1: Definir formulas
+-2: Implementar formas en JS
+-3: Crear funciones
+-4: Integrar JS con HTML
