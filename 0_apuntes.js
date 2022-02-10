42282e0c272a6b3ebb5a4d982682f46c754b4e2f
unknown <gustavogud77@gmail.com>
2022-02-10 18:07:26 -0400

    README

--- 0_apuntes.js
+++ 0_apuntes.js
@@ -4,5 +4,15 @@
 	NODE es un template para el .gitignore (.nombre para carpetas, nombre para archivos)
 	
 	README.md se utiliza para brindar una descripcion detallada del repositorio
-	Utiliza un lenguaje llamado markdown
+	Utiliza un lenguaje llamado markdown (.md Markdown documentation)
+	# h1, ## h2, ..., ###### h6
+	*text* o _text_ Italic
+	**text** o __text__ Bold
+	>text Block Quote
+	
+	Unordered Lists:
+	Item 1Item 2Item 3Item 4
+
+	Order Lists:
+	Item 1Item 2Item 3Item 4Item 5
 */
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # practicaJS
 
 ## Taller 1: figuras geometricas
+Order Lists:
 -1: Definir formulas
 -2: Implementar formas en JS
 -3: Crear funciones
 -4: Integrar JS con HTML
+4
